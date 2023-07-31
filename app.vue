6dda28699c2237262ee6197a273877a284e33812
Yari Herrera <yari.andres@gmail.com>
2023-07-31 23:34:13 +0200

    small changes

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="dark:bg-gray-900">
     <NuxtPage />
   </div>
 </template>
+
+<style>
+html {
+  background-color:#111827;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,8 @@
 		</nav>
 	</header>
 
-	<section class="bg-white dark:bg-gray-900 h-screen">
-		<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 h-screen">
+	<section class="bg-white dark:bg-gray-900">
+		<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 			<div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
 				<h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Hi I'm Yari</h2>
 				<p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
